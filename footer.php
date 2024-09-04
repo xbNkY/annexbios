f49a05241325bc4b38e3a395442d6320d821d80e
bNkY <70035512+xbNkY@users.noreply.github.com>
2024-09-04 15:52:42 +0200

    -
    -

---
+++ footer.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+</head>
+<body>
+
+<footer>
+    <div class="footer-container">
+        <div class="footer-nav">
+            <a href="#">Voorwaarden</a>
+            <a href="#">Privacy beleid</a>
+            <a href="#">Cookie disclaimer</a>
+        </div>
+        <!-- <div class="footer-social">
+            <a href="#" class="fa fa-facebook"></a>
+            <a href="#" class="fa fa-twitter"></a>
+            <a href="#" class="fa fa-instagram"></a>
+        </div> -->
+    </div>
+</footer>
+
+</body>
+</html>
+
