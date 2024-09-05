5fd76ce919f68b0d2f71e86d5372cd6cfb039606
bNkY <70035512+xbNkY@users.noreply.github.com>
2024-09-05 15:44:00 +0200

    Light edit
    -

--- core/header.php
+++ core/header.php
@@ -3,15 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/header.css">
+    <link rel="stylesheet" href="./assets/css/header.css">
+    <style>@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');</style>
     <title>AnnexBios - Maarssen</title>
 </head>
-<body>
+<body class="body">
     
         <div class="header-container">
             <div class="header-white-bar">
                 <div class="header-logo">
-                    <img src="../assets/images/logo/logo_hoofd.png">
+                    <img src="./assets/images/logo/logo_hoofd.png">
                     <div class="navbar">
                         
                     </div>
