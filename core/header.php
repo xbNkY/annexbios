7822bc9815282bba5bc4b5ce7338021b2c3ae7b8
bNkY <70035512+xbNkY@users.noreply.github.com>
2024-09-05 14:10:31 +0200

    -
    -

---
+++ core/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/header.css">
+    <title>AnnexBios - Maarssen</title>
+</head>
+<body>
+    
+        <div class="header-container">
+            <div class="header-white-bar">
+                <div class="header-logo">
+                    <img src="../assets/images/logo/logo_hoofd.png">
+                    <div class="navbar">
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="header-navbar">
+
+            </div>
+        </div>
+
+
+
+
+</body>
+</html>
