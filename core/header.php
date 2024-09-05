d85dbbec7dc46699ef54c7c7cc88ffa4334d5775
bNkY <70035512+xbNkY@users.noreply.github.com>
2024-09-05 15:56:22 +0200

    Fix
    Fix

--- core/header.php
+++ core/header.php
@@ -9,22 +9,25 @@
 </head>
 <body class="body">
     
-        <div class="header-container">
-            <div class="header-white-bar">
+    <div class="header">
+        <div class="container">
+            <div class="navbar">
                 <div class="header-logo">
-                    <img src="./assets/images/logo/logo_hoofd.png">
-                    <div class="navbar">
-                        
-                    </div>
-                </div>
-            </div>
-            <div class="header-navbar">
-
+                    <img src="assets/images/logo/logox.png" width="300px">
+                </div>    
             </div>
         </div>
 
-
-
-
+        <nav>
+                <ul id="MenuItems" style="max-height: 0px;">
+                    <li><a href="">FILM AGENDA</a></li>
+                    <li><a href="">ALLE VESTIGINGEN</a></li>
+                    <li><a href="">CONTACT</a></li>
+                </ul>
+            </nav>
+            <img src="assets/images/menu.png" class="menu-icon" onclick="menutoggle()">
+        </div>
+    </div>
+</div>
 </body>
 </html>
