9436d01abdd589016337c7f4d843c7160914a923
evangelion08 <evacardelldevries@gmail.com>
2024-09-05 15:43:46 +0200

    header

--- core/header.php
+++ core/header.php
@@ -1,25 +1,24 @@
-
 <link rel="stylesheet" href="assets/css/header.css">
 
 <div class="header">
-  <div class="container">
-    <div class="navbar">
-      <div class="logo">
-        <img src="assets/images/logo/logo_hoofd.png" width="125px">
-      </div>
+    <div class="container">
+        <div class="navbar">
+            <div class="logo">
+                <img src="assets/images/logo/logox.png" width="300px">
+            </div>
 
-      <nav>
-        <ul id="MenuItems" style="max-height: 0px;">
-          <li><a href="">FILM AGENDA</a></li>
-          <li><a href="">ALLE VESTIGINGEN</a></li>
-          <li><a href="">CONTACT</a></li>
-        </ul>
-      </nav>
-      <img src="assets/images/menu.png" class="menu-icon" onclick="menutoggle()">
-
-
+            <nav>
+                <ul id="MenuItems" style="max-height: 0px;">
+                    <li><a href="">FILM AGENDA</a></li>
+                    <li><a href="">ALLE VESTIGINGEN</a></li>
+                    <li><a href="">CONTACT</a></li>
+                </ul>
+            </nav>
+            <img src="assets/images/menu.png" class="menu-icon" onclick="menutoggle()">
 
 
+
+
+        </div>
     </div>
-  </div>
 </div>
