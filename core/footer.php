c429f22ec7fa9dd6b4cef92cd814decf6aa08c54
evangelion08 <evacardelldevries@gmail.com>
2024-09-10 15:23:38 +0200

    header bijna af

--- core/footer.php
+++ core/footer.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href=".assets/css/footer.php">
+    <link rel="stylesheet" href="annexbios/assets/css/fonts.css">
     <style>@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');</style>
     <title>Document</title>
 </head>
--- core/header.php
+++ core/header.php
@@ -1,40 +1,63 @@
-<<<<<<< HEAD
-
-<div class="header">
-=======
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/header.css">
-    <style>@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');</style>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
+    </style>
     <title>AnnexBios - Maarssen</title>
 </head>
+
 <body class="body">
-  
+
+
+
     <div class="header">
->>>>>>> 34c0aa7f2916132d4501aae2a0c15b0af397fea2
-    <div class="container">
-        <div class="navbar">
-            <div class="header-logo">
-                <img src="assets/images/logo/logox.png" width="300px">
+        <div class="container">
+            <div class="navbar">
+                <div class="header-logo">
+                    <img src="assets/images/logo/logox.png">
+                </div>
+
+                <nav>
+                    <ul id="MenuItems" style="max-height: 0px;">
+                        <li><a href="">FILM AGENDA</a></li>
+                        <li><a href="">ALLE VESTIGINGEN</a></li>
+                        <li><a href="">CONTACT</a></li>
+                    </ul>
+                </nav>
+
             </div>
-
-            <nav>
-                <ul id="MenuItems" style="max-height: 0px;">
-                    <li><a href="">FILM AGENDA</a></li>
-                    <li><a href="">ALLE VESTIGINGEN</a></li>
-                    <li><a href="">CONTACT</a></li>
-                </ul>
-            </nav>
-            <img src="assets/images/menu.png" class="menu-icon" onclick="menutoggle()">
-
         </div>
     </div>
+
+    <div class="second-header">
+        <div class="h1">KOOP JE TICKETS</div>
+
+        <button type="button" class="button">BESTEL TICKETS</button>
+    </div>
+
+
+    <div class="dropdown">
+    <button class="dropbtn">
+        Kies je film
+        <span class="arrow">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+                <polyline points="40,40 50,50 60,40" style="fill:none;stroke:white;stroke-width:4;" />
+            </svg>
+        </span>
+    </button>
+    <div class="dropdown-content">
+        <a href="#">Film 1</a>
+        <a href="#">Film 2</a>
+        <a href="#">Film 3</a>
+    </div>
 </div>
-<<<<<<< HEAD
-=======
+
+
 </body>
+
 </html>
->>>>>>> 34c0aa7f2916132d4501aae2a0c15b0af397fea2
