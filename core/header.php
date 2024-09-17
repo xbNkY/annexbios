604bde51f53e23ac3ce134b40de3658e4be5c7ed
Dalysha <Dalyshajonis@outlook.com>
2024-09-17 13:35:56 +0200

    comkiyt

--- core/header.php
+++ core/header.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/header.css">
     <link rel="stylesheet" href="./assets/css/footer.css">
-    <link rel="stylesheet"  href="assets\css\home.css">
+    <link rel="stylesheet"  href="./assets\css\home.css">
+    <link rel="stylesheet"  href="./assets\css\detail.css">
     <style>@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');</style>
     <title>AnnexBios - Maarssen</title>
 </head>
