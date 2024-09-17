2f0f198816403fd989d4f0d2056030050ca5b1f2
bNkY <70035512+xbNkY@users.noreply.github.com>
2024-09-17 13:22:39 +0200

    -
    -

--- home.php
+++ home.php
@@ -4,6 +4,7 @@ include('./core/header.php');
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,43 +12,74 @@ include('./core/header.php');
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>AnnexBios - Home</title>
 </head>
+
 <body>
 
-<div class="container">
-    <!-- Header Section -->
-    <div class="header-section">
-        <h2>WELKOM BIJ ANNEXBIOS 2</h2>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil maxime odio at dolorem hic, deserunt enim exercitationem modi vel architecto, omnis placeat explicabo tempora.</p>
-        <button class="button">BEKIJK DE DRAAIENDE FILMS</button>
+    <script>(g => {
+            var h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document, b = window; b = b[c] || (b[c] = {}); var d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams, u = () => h || (h = new Promise(async (f, n) => {
+                await (a = m.createElement("script")); e.set("libraries", [...r] + ""); for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]); e.set("callback", c + ".maps." + q); a.src = `https://maps.${c}apis.com/maps/api/js?` + e; d[q] = f; a.onerror = () => h = n(Error(p + " could not load.")); a.nonce = m.querySelector("script[nonce]")?.nonce || ""; m.head.append(a)
+            })); d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n))
+        })
+            ({ key: "AIzaSyCdrSau_AMiwHsCBTcu5tJehukVHOx1-h4", v: "weekly" });</script>
+
+    <div class="home-container">
+        <!-- Header Section -->
+        <div class="header-section">
+            <h2>WELKOM BIJ ANNEXBIOS 2</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil maxime odio at dolorem hic, deserunt enim
+                exercitationem modi vel architecto, omnis placeat explicabo tempora.</p>
+            <button class="button">BEKIJK DE DRAAIENDE FILMS</button>
+        </div>
+
+        <div class="flex-section">
+            <div id="map"></div>
+            <div class="flex-tivoli">
+                <!-- <img src="./assets/images/header/tivolio.png"> -->
+            </div>
+            <div class="flex-details">
+                <i class="fa fa-map-marker"><span> Rijksstraatweg 42 <br> 3223 KA Hellevoetsluis</span></i> <br>
+                <i class="fa fa-phone"><span> 020-12345678</span></i> <br>
+                <h4>BEREIKBAARHEID</h4>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, ad soluta pariatur nobis sint
+                    repellendus laudantium debitis sed facere distinctio animi magnam et quaerat tenetur tempora
+                    repellat cum aperiam consequuntur!</p>
+            </div>
+        </div>
+
     </div>
 
-    <div class="flex-section">
-        <div id="googleMap" style="width:100%l;height:400px;"></div>
-        <div class="flex-tivoli">
-            <!-- <img src="./assets/images/maps/tivolitheater.jpg"> -->
-        </div>
-        <div class="flex-details">
-            <i class="fa fa-map-marker"><span> Rijksstraatweg 42 <br> 3223 KA Hellevoetsluis</span></i> <br>
-            <i class="fa fa-phone"><span> 020-12345678</span></i> <br>
-            <h4>BEREIKBAARHEID</h4>
-            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, ad soluta pariatur nobis sint repellendus laudantium debitis sed facere distinctio animi magnam et quaerat tenetur tempora repellat cum aperiam consequuntur!</p>
-        </div>
-    </div>
-    
-</div>
+    <script>
+        let map;
 
-<script>
-                    function myMap() {
-                        var mapProp = {
-                            center:new google.maps.LatLng(51.8355401, 4.1328773),
-                            zoom:5,
-                        };
-                        var map = new google.maps.map(document.gelElementByID("googleMap"),mapProp);
-                    };
-                </script>
-                <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY&callback=myMap"></script>
+        async function initMap() {
+
+            const position = { lat: 51.8355401, lng: 4.1328773 };
+
+
+            const { Map } = await google.maps.importLibrary("maps");
+            const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+
+            map = new Map(document.getElementById("map"), {
+                zoom: 17,
+                center: position,
+                mapId: "Rijksstraatweg 42",
+                disableDefaultUI: true,
+            });
+
+
+            const marker = new AdvancedMarkerElement({
+                map: map,
+                position: position,
+                title: "Rijksstraatweg 42",
+            });
+        }
+
+        initMap();
+    </script>
 
 </body>
+
 </html>
 
 <?php
