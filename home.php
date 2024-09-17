518adda9b34cf35d0806d77a4d7026319799c3d0
evangelion08 <evacardelldevries@gmail.com>
2024-09-17 13:29:04 +0200

    eindelijk gefixt

--- home.php
+++ home.php
@@ -3,18 +3,9 @@ include('./core/header.php');
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/home.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>AnnexBios - Home</title>
-</head>
 <body>
 
-<div class="container">
+<div class="home-container">
     <!-- Header Section -->
     <div class="header-section">
         <h2>WELKOM BIJ ANNEXBIOS 2</h2>
