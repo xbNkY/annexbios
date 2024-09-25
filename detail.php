276e51c559b4d89e0db75366a0fdba8327598756
Dalysha <Dalyshajonis@outlook.com>
2024-09-25 13:42:29 +0200

    changes

--- detail.php
+++ detail.php
@@ -10,9 +10,7 @@ include('./core/header.php');
     </div>
     <div class="detail_container">
 
-
-       
-            <img id="detail_foto" src="assets/images/films/Jurassic-World_-Fallen-Kingdom.jpg">
+       <img id="detail_foto" src="assets/images/films/Jurassic-World_-Fallen-Kingdom.jpg">
  
 
         <div class="details_film">
