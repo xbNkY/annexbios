cd519a1973dff668e8d5b7f9a6619dc08ad6cd6e
Danny <danny@bonky.dev>
2024-09-25 13:35:22 +0200

    Film agenda
    API request werkt soort van, check op school met docent. Error 403 op thuis netwerk.

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
 include('./core/header.php');
+include('./data/fetch_movies.php');
 ?>
 
-
+<?php
+try {
+    $movies = fetchMovieData();
+} catch (Exception $e) {
+    $error = $e->getMessage();
+}
+?>
 
 <div class="grid-container index-body">
 
@@ -19,15 +26,12 @@ include('./core/header.php');
             <h2>WELKOM BIJ ANNEXBIOS 2</h2>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil maxime odio at dolorem hic, deserunt enim
                 exercitationem modi vel architecto, omnis placeat explicabo tempora.</p>
-            <button class="button">BEKIJK DE DRAAIENDE FILMS</button>
+            <a href="showall.php"><button class="button">BEKIJK DE DRAAIENDE FILMS</button></a>
         </div>
 
         <div class="flex-section">
             
-            <div class="flex-tivoli">
-                
-                <img src="./assets/images/header/tivolio.png">
-            </div>
+            
             <div class="flex-details">
                 <div id="map"></div>
                 <i class="fa fa-map-marker"><span> Rijksstraatweg 42 <br> 3223 KA Hellevoetsluis</span></i> <br>
@@ -37,6 +41,34 @@ include('./core/header.php');
                     repellendus laudantium debitis sed facere distinctio animi magnam et quaerat tenetur tempora
                     repellat cum aperiam consequuntur!</p>
             </div>
+
+            <div class="flex-tivoli">
+                
+                <img src="./assets/images/header/tivolio.png">
+            </div>
+        </div>
+        
+        <div class="movie-agenda-section">
+            <h2>Movie Film Agenda</h2>
+            <?php if(isset($error)): ?>
+                <p class="error-message"><?php echo htmlspecialchars($error); ?></p>
+            <?php else: ?>
+                <div class="movies-grid">
+                    <?php foreach($movies as $movie): ?>
+                        <div class="movie-card">
+                            <img src="<?php echo htmlspecialchars($movie['image']); ?>" alt="<?php echo htmlspecialchars($movie['title']); ?>">
+                            <h3><?php echo htmlspecialchars($movie['title']); ?></h3>
+                            <p><?php echo htmlspecialchars($movie['description']); ?></p>
+                            <p><strong>Release Date:</strong> <?php echo htmlspecialchars(date("d M Y", strtotime($movie['release_date']))); ?></p>
+                            <p><strong>Rating:</strong> <?php echo htmlspecialchars($movie['rating']); ?>/10</p>
+                            <p><strong>Duration:</strong> <?php echo htmlspecialchars($movie['length']); ?> minutes</p>
+                            <a href="<?php echo htmlspecialchars($movie['trailer_link']); ?>" target="_blank">
+                                <button class="button">MEER INFO & TICKETS</button>
+                            </a>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            <?php endif; ?>
         </div>
 
     </div>
